cc428056c5387b78901f28321e13a3cd7f21ca9b
Bruce <brucehrwang@yahoo.com>
2018-09-17 15:24:10 -0400

    initial commit

---
+++ Score.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    public static int scoreValue = 0;
+    Text score;
+
+
+    void Start()
+    {
+        score = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update () {
+        score.text = "Score: " + scoreValue;
+	}
+}
---
+++ Target.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public class Target : MonoBehaviour
+{
+
+    public float health = 50f;
+    public GameObject destroyedVersion;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Score.scoreValue += 1;
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
